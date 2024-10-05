8d5a5c65288d50f960407ca1e64622f5b4c884e7
Christophe <christophegauthiersokpoly@gmail.com>
2024-10-05 12:39:38 +0100

    update

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ var swiper = new Swiper(".slider", {
     clickable: true,
   },
 });
+
+
+let menu = document.querySelector('#menu-icon');
+let menulist = document.querySelector('.menulist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  menulist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  menulist.classList.remove('open');
+};
